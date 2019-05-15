d0099a8221e305fd6c514016bbc4d57294a7662d
ezb2018 <47731051+ezb2018@users.noreply.github.com>
2019-05-15 15:50:27 +0800

    Create breadcrumbs.js

---
+++ breadcrumbs.js
@@ -0,0 +1,29 @@
+let item = [];
+let count = 1;
+let el = document.createElement('script');
+el.type = 'application/ld+json';
+
+if ($(".google-breadcrumb").find('li').length > 0) { 
+    $(".google-breadcrumb").find('li').each(function () {
+        let url = '';
+        if ($(this).find('a').attr('href') == undefined)
+            url = window.location.pathname;
+        else
+            url = $(this).find('a').attr('href');
+
+        item.push({
+            "@type": "ListItem",
+            "position": count,
+            "name": $(this).text(),
+            "item": "https://" + window.location.host + url
+        });
+        count++;
+    });
+
+    el.text = JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": item
+    });
+    document.querySelector('body').appendChild(el);
+}
